97dfe074017be4d2ab33759242d56282f4b98ea1
Moshe Deitsch <me@moshedeitsch.com>
2020-01-28 20:19:52 +0200

    arrow function app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css'
+import { DarkModeContext } from './context/DarkModeContext';
 
-function App() {
+import Navbar from './components/Navbar';
+import Form from './components/Form';
+
+
+const App = () => {
+const context = React.useContext(DarkModeContext);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={context.darkMode ? "dark-mode" : "light-mode"}>
+      <Navbar/>
+      <Form/>
+  
     </div>
-  );
+  )
 }
 
 export default App;
