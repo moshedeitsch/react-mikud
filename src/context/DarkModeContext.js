220172391fdb9ed2a1e5ef144560d245d3002747
Moshe Deitsch <me@moshedeitsch.com>
2020-01-28 20:20:47 +0200

    add dark mode

---
+++ src/context/DarkModeContext.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export const DarkModeContext = React.createContext()
+
+
+
+const DarkModeProvider = (props) => {
+    const [darkMode, setDarkMode] = React.useState(getInitialMode())
+
+
+    React.useEffect(() => {
+        localStorage.setItem('dark', JSON.stringify(darkMode))
+    }, [darkMode])
+    
+    function getInitialMode() {
+        const isReturningUser = "dark" in localStorage;
+        const savedMode = JSON.parse(localStorage.getItem("dark"));
+        const userPrefersDark = getPrefColorScheme();
+        // if mode was saved --> dark / light
+        if (isReturningUser) {
+            return savedMode;
+            // if preferred color scheme is dark --> dark
+        } else if (userPrefersDark) {
+            return true;
+            // otherwise --> light
+        } else {
+            return false;
+        }
+        // return savedMode || false;
+    }
+    
+    
+    function getPrefColorScheme() {
+        if (!window.matchMedia) return;
+    
+        return window.matchMedia("(prefers-color-scheme: dark)").matches;
+    }
+    
+     return (
+         <DarkModeContext.Provider value={{darkMode,setDarkMode}} >
+             { props.children }
+         </DarkModeContext.Provider>
+     )
+ 
+ }
+ 
+ export default DarkModeProvider
