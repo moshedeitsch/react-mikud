1f6ed7dadddb9916a495627b1e16f81c839794b0
Moshe Deitsch <me@moshedeitsch.com>
2020-02-24 17:33:44 +0200

    fix css

--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -1,4 +1,4 @@
-import React,{useContext} from 'react'
+import React, { useContext } from 'react'
 import '../style/../style/result.css'
 import { FormContext } from '../context/FormContext'
 
@@ -6,15 +6,27 @@ const Result = (props) => {
     const context = useContext(FormContext);
 
     const loading = context.mikud.loading
-    const zipCode = context.mikud.mikud.data  
+    const zipCode = context.mikud.mikud.data
 
-    return ( 
+
+
+    return (
         <div className='result'>
-            {loading ? <div className="loader"><div></div><div></div></div> : loading }
-            
-            {zipCode ? <div><span>המיקוד לכתובת הינו: {zipCode}</span></div> : " " }
+            {loading ? <div className="loader"><div></div><div></div></div> : loading}
+
+            {zipCode ? <div style={{display: 'inline-grid'}}>
+                <span>המיקוד לכתובת הינו: {zipCode}</span>
+                <br/>
+                <button  onClick={() => { navigator.clipboard.writeText(zipCode) }}>Copy</button>
+
+                </div> : " "}
+
+
+
+
+
         </div>
-     );
+    );
 }
- 
+
 export default Result; 
